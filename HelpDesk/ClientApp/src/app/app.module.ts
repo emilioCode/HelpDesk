0db94702aef14e8850789249a45bbf2517739993
Manuel E Mendez <emilio_mem@hotmail.com>
2020-10-29 20:50:19 -0400

    init template

--- HelpDesk/ClientApp/src/app/app.module.ts
+++ HelpDesk/ClientApp/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { ContentComponent } from './components/shared/content/content.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { SettingComponent } from './components/shared/setting/setting.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
+import { LoginComponent } from './components/shared/login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,13 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    SettingComponent,
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
