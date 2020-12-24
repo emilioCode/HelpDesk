75f520136cbdb4cd32a8a5ef8f9620adf312d82f
Manuel E Mendez <emilio_mem@hotmail.com>
2020-12-24 19:28:36 -0400

    i made the Encrypt Page for encry

---
+++ HelpDesk/Pages/Encrypt.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelpDesk.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelpDesk.Pages
+{
+    public class EncryptModel : PageModel
+    {
+        public string request { get; set; }
+        public string result { get; set; }
+        public void OnGet()
+        {
+            request = "";
+        }
+
+        public void OnPost(string pwd)
+        {
+            result = pwd != "" ? Security.Encripting(pwd) : "";
+        }
+    }
+}
