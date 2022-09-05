9408ac1a383380d68e5a163785a0c546b9abcffd
Manuel E Mendez <emilio_mem@hotmail.com>
2022-09-05 00:37:20 -0400

    Removing MailClient class from Controller's folder

--- HelpDesk.Core/Services/TicketService.cs
+++ HelpDesk.Core/Services/TicketService.cs
@@ -4,7 +4,6 @@ using HelpDesk.Core.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace HelpDesk.Core.Services
